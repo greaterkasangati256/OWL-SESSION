00b4f37813f031fd060604fea098e114e100fcf1
KASANGATI GREATER <141672932+greaterkasangati256@users.noreply.github.com>
2025-09-15 17:20:26 +0300

    Add files via upload

---
+++ Backend.js
@@ -0,0 +1,11 @@
+{
+  "name": "whatsapp-google-bot",
+  "version": "1.0.0",
+  "main": "bot.js",
+  "type": "module",
+  "dependencies": {
+    "express": "^4.18.2",
+    "body-parser": "^1.20.2",
+    "googleapis": "^133.0.0"
+  }
+}
